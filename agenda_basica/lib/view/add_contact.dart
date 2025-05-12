e97e7f3e090538c88ba03e447222561cf1644ed5
Rebecca Nery <112131032+RebeccaNery@users.noreply.github.com>
2025-05-12 00:51:29 -0300

    finalmente, projeto agenda_basica criado corretamente

---
+++ agenda_basica/lib/view/add_contact.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AddContact extends StatefulWidget {
+  const AddContact({super.key});
+
+  @override
+  State<AddContact> createState() => _AddContactState();
+}
+
+class _AddContactState extends State<AddContact> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Adicionar Contato"),
+      ),
+      body: TextButton(
+        onPressed: (){
+          Navigator.pop(context);
+        },
+        child: Text("Close")
+      )
+    );
+  }
+}
